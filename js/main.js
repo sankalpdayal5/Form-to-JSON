33937a73e23886b8b3f4ea373a923c7579130b3f
Sankalp Dayal <sankalpdayal5@gmail.com>
2018-05-11 16:05:39 +0530

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
+var b_title = document.getElementById('title');
+var b_img_url = document.getElementById('img_url');
+var b_bp1 = document.getElementById('bp1');
+var b_bp2 = document.getElementById('bp2');
+var b_bp3 = document.getElementById('bp3');
+var b_author_name = document.getElementById('author_name');
+
 function isValid(){
-    if (blog.title.value == '' || blog.img_url.value == '' || blog.bp1.value == '' || blog.author_name.value == ''){
-        alert ('Enter the required fields correctly');
-    }else{
+
+    // console.log(b_title.value);
+    // console.log(b_img_url.value);
+    // console.log(b_bp1.value);
+    // console.log(b_bp2.value);
+    // console.log(b_bp3.value);
+    // console.log(b_author_name.value);
+    
+    var re1 = /^((https?|ftp):)?\/\/.*(jpeg|jpg|png|gif|bmp)/;
+
+    if (re1.test(b_img_url.value)){
+        console.log('valid url 1');
         makeJson();
+    }else{
+        alert('Enter valid url for the image')
+        console.log('invalid url');
     }
+
+    
+
+    return false;
+
+    
 };
 
 function makeJson(){
